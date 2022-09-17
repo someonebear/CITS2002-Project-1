98b2f4e0d7ad251ef212b9ade72cb9995c29ae6e
Zhihao <zhi.lin1@outlook.com>
2022-09-17 12:12:44 +0800

    Commit for sharing, problem with shallow copy of pointers

--- estimatecron.c
+++ estimatecron.c
@@ -19,23 +19,32 @@ void get_date(char line[], char *buf[]) {
 int main(int argc, char *argv[]) {
   FILE *dict;
   char line[100];
-  char *date[5];
+  char *date[6];
+  char *tasks[120];
 
   dict = fopen("crontab-file.txt", "r");
   if (dict==NULL) {
     printf("error opening file\n");
   }
-
+  int j = 0;
+  char temp;
   while(fgets(line, sizeof line, dict) != NULL) {
     if (line[0] == '#') {
       continue;
     }
     get_date(line, date);
-    for (int i=0; i<5; i++){
-      printf("%s\n", date[i]);
-    }
-  }
+    for (int i=0; i<6; i++){
+      temp = *date[i];
+      // printf("%s\n", temp);
+      // *tasks[j*6+1] = temp;
+      // printf("%s\n", date[i]);
+      // printf("%s\n", tasks[j*6+i]);
 
+    }
+
+    j = j + 1;
+  }
+  // printf("%s\n", tasks[10]);
   fclose(dict);
   return 0;
 }
