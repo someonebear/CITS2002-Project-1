c54a86f43794150205a4193308efc67b464c8308
Zhihao <zhi.lin1@outlook.com>
2022-09-18 18:06:34 +0800

    Add every line to big array

--- estimatecron.c
+++ estimatecron.c
@@ -16,6 +16,8 @@ void get_date(char line[], char *buf[]) {
   }
 }
 
+ 
+
 int main(int argc, char *argv[]) {
   FILE *dict;
   char line[100];
@@ -26,25 +28,27 @@ int main(int argc, char *argv[]) {
   if (dict==NULL) {
     printf("error opening file\n");
   }
+
   int j = 0;
-  char temp;
   while(fgets(line, sizeof line, dict) != NULL) {
     if (line[0] == '#') {
       continue;
     }
     get_date(line, date);
     for (int i=0; i<6; i++){
-      temp = *date[i];
-      // printf("%s\n", temp);
-      // *tasks[j*6+1] = temp;
-      // printf("%s\n", date[i]);
-      // printf("%s\n", tasks[j*6+i]);
-
+      tasks[j*6+i] = (char *) malloc(sizeof(date[i])+1);
+      strcpy(tasks[j*6+i], date[i]);
     }
-
     j = j + 1;
   }
-  // printf("%s\n", tasks[10]);
+
+  // for (int k=0; k<5; k++) {
+  //   for (int l=0; l<6; l++){
+  //     printf("%s\n", tasks[k*6+l]);
+  //   }
+  // }
+
   fclose(dict);
+  free(tasks);
   return 0;
 }
