b4ad2ee29b0abb8cd0d54ffe2919499cc6ae7a95
Zhihao <zhi.lin1@outlook.com>
2022-09-19 20:20:39 +0800

    Read estimates file into program

--- estimatecron.c
+++ estimatecron.c
@@ -10,6 +10,13 @@
 #define HOUR_INDEX 1
 #define MINUTE_INDEX 0
 
+struct Process {
+  char pname[50];
+  int pid;
+  int runtime;
+  int running;
+};
+
 void get_date(char line[], char *buf[]) {
   char* ptr = strtok(line, " ");
   int i = 0;
@@ -39,7 +46,7 @@ void simulate(char *tasks[], int month, int* total_command, int* max_command, ch
       if (tasks[i*6+TASK_NAME_INDEX] == NULL) {
         break;
       } else if (*tasks[i*6+DAY_INDEX] == day || *tasks[i*6+DAY_INDEX] == '*') {
-        printf("The day is %i, and the task is %s\n", day, tasks[i*6+TASK_NAME_INDEX]);
+        // printf("The day is %i, and the task is %s\n", day, tasks[i*6+TASK_NAME_INDEX]);
       }
     } 
     // printf("Minute:%i, Hour:%i, Day:%i", minutes, hours, day);
@@ -50,19 +57,21 @@ void simulate(char *tasks[], int month, int* total_command, int* max_command, ch
 
 
 int main(int argc, char *argv[]) {
-  FILE *dict;
+  FILE *schedule, *estimates;
   char line[100];
   char *date[6];
   char *tasks[120] = {NULL};
+  char *task_times[20] = {NULL};
 
-  dict = fopen("crontab-file.txt", "r");
-  if (dict==NULL) {
+  schedule = fopen("crontab-file.txt", "r");
+  estimates = fopen("estimates-file.txt", "r");
+  if (schedule==NULL || estimates == NULL) {
     printf("error opening file\n");
   }
 
   // Reading file into array
   int j = 0;
-  while(fgets(line, sizeof line, dict) != NULL) {
+  while (fgets(line, sizeof line, schedule) != NULL) {
     if (line[0] == '#') {
       continue;
     }
@@ -74,20 +83,33 @@ int main(int argc, char *argv[]) {
     j = j + 1;
   }
 
+  int m = 0;
+  while (fgets(line, sizeof line, estimates) != NULL) {
+    if (line[0] == '#') {
+      continue;
+    }
+    get_date(line, date);
+    for (int n=0; n<2; n++) {
+      task_times[m*2+n] = (char *) malloc(sizeof(date[n])+1);
+      strcpy(task_times[m*2+n], date[n]);
+    }
+    m = m + 1;
+  }
+
   int *total_command = 0;
   int *max_command = 0;
   char *most_command = (char *) malloc(20);
   simulate(tasks, *argv[1], total_command, max_command, most_command);
   // for (int k=0; k<5; k++) {
-  //   for (int l=0; l<6; l++){
-  //     if (tasks[k*6+l] == NULL) {
+  //   for (int l=0; l<2; l++){
+  //     if (task_times[k*2+l] == NULL) {
   //       break;
   //     }
-  //     printf("%s\n", tasks[k*6+l]);
+  //     printf("%s\n", task_times[k*2+l]);
   //   }
   // }
 
-  fclose(dict);
+  fclose(schedule);
   return 0;
 }
 
