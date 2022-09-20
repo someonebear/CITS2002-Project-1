a21cfbadcb9e28b2d4455a9082ba8f9398f1051b
Zhihao <zhi.lin1@outlook.com>
2022-09-20 17:01:44 +0800

    Change while loop to change condition based on month

--- estimatecron.c
+++ estimatecron.c
@@ -109,8 +109,10 @@ void simulate(char *tasks[], char *task_estimates[], char *month, int* total_com
   int minutes = 0;
   int hours = 0;
   int day = 1;
+  int days_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int current_month = return_month(month);
 
-  while (day<31) {
+  while (day < days_in_month[current_month] + 1) {
     if (minutes == 60) {
       hours = hours + 1;
       minutes = 0;
@@ -124,20 +126,15 @@ void simulate(char *tasks[], char *task_estimates[], char *month, int* total_com
       if (tasks[i*6+TASK_NAME_INDEX] == NULL) {
         break;
       } else if (is_now(tasks[i*6+MINUTE_INDEX], minutes) == 1) {
-        // printf("%s is equal to %i\n", tasks[i*6+MINUTE_INDEX], minutes);
         if (is_now(tasks[i*6+HOUR_INDEX], hours) == 1) {
           if (is_now(tasks[i*6+DAY_INDEX], day) == 1) {
             if (check_month(tasks[i*6+MONTH_INDEX], month) == 1) {
               if (check_weekday(tasks[i*6+WEEKDAY_INDEX], month, day) == 1) {
                 printf("The day is %i, and the task is %s\n", day, tasks[i*6+TASK_NAME_INDEX]);
               }
-              
             }
           } 
-        } else {
-          // printf("File is %s, hour is %i", tasks[i*6+HOUR_INDEX], hours);
         }
-        // printf("The day is %i, and the task is %s\n", day, tasks[i*6+TASK_NAME_INDEX]);
       }
     } 
     minutes = minutes + 1;
@@ -163,58 +160,25 @@ void parse_file(FILE *ptr, char *buf[], char *dest[], int columns) {
 
 int main(int argc, char *argv[]) {
   FILE *schedule, *estimates;
-  // char line[100];
   char *date[6];
   char *tasks[120] = {NULL};
   char *task_times[20] = {NULL};
 
   schedule = fopen("crontab-file.txt", "r");
   estimates = fopen("estimates-file.txt", "r");
-  if (schedule==NULL || estimates == NULL) {
+  if (schedule == NULL || estimates == NULL) {
     printf("error opening file\n");
   }
 
   // Reading file into array
   parse_file(schedule, date, tasks, 6);
   parse_file(estimates, date, task_times, 2);
-  // int j = 0;
-  // while (fgets(line, sizeof line, schedule) != NULL) {
-  //   if (line[0] == '#') {
-  //     continue;
-  //   }
-  //   get_date(line, date);
-  //   for (int i=0; i<6; i++){
-  //     tasks[j*6+i] = (char *) malloc(sizeof(date[i])+1);
-  //     strcpy(tasks[j*6+i], date[i]);
-  //   }
-  //   j = j + 1;
-  // }
-
-  // int m = 0;
-  // while (fgets(line, sizeof line, estimates) != NULL) {
-  //   if (line[0] == '#') {
-  //     continue;
-  //   }
-  //   get_date(line, date);
-  //   for (int n=0; n<2; n++) {
-  //     task_times[m*2+n] = (char *) malloc(sizeof(date[n])+1);
-  //     strcpy(task_times[m*2+n], date[n]);
-  //   }
-  //   m = m + 1;
-  // }
-
+  
   int *total_command = 0;
   int *max_command = 0;
   char *most_command = (char *) malloc(40);
   simulate(tasks, task_times, argv[1], total_command, max_command, most_command);
-  // for (int k=0; k<5; k++) {
-  //   for (int l=0; l<2; l++){
-  //     if (task_times[k*2+l] == NULL) {
-  //       break;
-  //     }
-  //     printf("%s\n", task_times[k*2+l]);
-  //   }
-  // }
+  
 
   fclose(schedule);
   fclose(estimates);
